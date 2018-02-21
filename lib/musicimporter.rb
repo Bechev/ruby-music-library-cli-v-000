5835bbf2a4e6ef863017dce04a38bfcc3014edba
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-21 02:13:22 +0000

    automatically backed up by learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -11,7 +11,7 @@ class MusicImporter
   end
 
   def import
-    file_import = self.files.each{ |file| Song.create_from_filename(file)}
     binding.pry
+    file_import = self.files.each{ |file| Song.create_from_filename(file)}
   end
 end
