b0a78a2bbfd6d5ec19cbb2f1b382a615b837ccb1
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-21 02:19:55 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -29,7 +29,7 @@ attr_accessor :path, :imported_music
 
     Song.all.sort_by!{|song| song.name.split(" - ")[1]}
 
-    Song.all.each {|song,index| puts "#{index }. #{song}"}
+    Song.all.each {|song,index| puts "#{index }. #{song.name}"}
     #@imported_music.sort_by!{|song| song.split(" - ")[1]}
     #count = 1
     #@imported_music.each do |song| puts "#{count}. #{song}"
