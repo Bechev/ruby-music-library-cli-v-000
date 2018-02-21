ec5b25d630dd2954aca5525d22aa221f2ac0b4db
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-21 02:27:53 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -28,7 +28,7 @@ attr_accessor :path, :imported_music
   def list_songs
 
     song_list = Song.all.sort_by!{|song| song.name}
-    binding.pry
+    #binding.pry
     song_list.each_with_index do |song,index|
       puts"#{index +1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
