35c402385d291608c95b5157a4fa2cd3b41b9946
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-21 02:32:49 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -36,7 +36,7 @@ attr_accessor :path, :imported_music
   def list_artists
     artist_list = Artist.all.sort_by{|artist| artist.name}
     artist_list.each_with_index do |song,index|
-      puts"#{index +1}. #{song.artist.name}"
+      puts"#{index +1}. #{artist.name}"
     end
   end
 
